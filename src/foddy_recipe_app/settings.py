1bbefa7997fb9f4c1980193c93ac362227dc0824
dipto0321 <diptokmk47@gmail.com>
2020-02-25 02:56:03 +0600

    :sparkles: Initial user app created
    - Remove unnesessary files
    - Add apps in settings

--- src/foddy_recipe_app/settings.py
+++ src/foddy_recipe_app/settings.py
@@ -26,7 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'rest_framework',
+    'core',
+    'user'
 ]
 
 MIDDLEWARE = [
