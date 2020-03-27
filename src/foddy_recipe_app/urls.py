0820e8892b41b09d1badc777991db885cbf0fcd9
dipto0321 <diptokmk47@gmail.com>
2020-03-28 02:22:03 +0600

    :rotating_light: Fix urls linter issue

--- src/foddy_recipe_app/urls.py
+++ src/foddy_recipe_app/urls.py
@@ -18,8 +18,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/doc/', schema_view.with_ui('swagger',
-                                         cache_timeout=0), name='schema-swagger-ui'),
+    path('api/doc/',
+         schema_view.with_ui('swagger',
+                             cache_timeout=0),
+         name='schema-swagger-ui'),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
