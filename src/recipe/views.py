e47e643d35b5df84fd7e6438ea75dc0205463a6c
dipto0321 <diptokmk47@gmail.com>
2020-03-28 03:28:32 +0600

    :recycle: Refactor recipe viewsets
    Remove duplications for tags and ingredients viewset

--- src/recipe/views.py
+++ src/recipe/views.py
@@ -1,18 +1,16 @@
-from core.models import Tag, Ingredient
+from core.models import Ingredient, Tag
 from recipe import serializers
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
-class TagViewset(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseRecipeAttrViewset(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """Base class for recipe attributes"""
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
         """Return objects for the current authenticated user"""
@@ -23,19 +21,13 @@ class TagViewset(viewsets.GenericViewSet,
         serializer.save(user=self.request.user)
 
 
-class IngredientViewset(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class TagViewset(BaseRecipeAttrViewset):
+    """Manage tags in the database"""
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class IngredientViewset(BaseRecipeAttrViewset):
     """Manage ingredients in the database"""
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
-
-    def get_queryset(self):
-        """Return ingredient objects for current authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Creating an ingredient"""
-        serializer.save(user=self.request.user)
