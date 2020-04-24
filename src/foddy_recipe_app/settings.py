3fb84213688fcf1559d41a88066ce2ece3c04256
dipto0321 <diptokmk47@gmail.com>
2020-04-24 18:52:51 +0600

    :wrench: Change settings.py
    - Allowed host make dynamic

--- src/foddy_recipe_app/settings.py
+++ src/foddy_recipe_app/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEGUB", default=False)
 
-ALLOWED_HOSTS = ["foody-recipe-app-api.herokuapp.com"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS_URL", default=[])
 
 # Application definition
 
