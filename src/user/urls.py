d90f486e20e7a40fbee0514d7d609f36ca7dee7b
dipto0321 <diptokmk47@gmail.com>
2020-05-19 06:21:57 +0600

    :fire: Remove jwt token custom views and serializer

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 from user import views
 
 app_name = 'user'
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
-    path('token/', views.TokenObtainPairView.as_view(), name='token'),
+    path('token/', TokenObtainPairView.as_view(), name='token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', views.ManageUserView.as_view(), name='me'),
 ]
