4b06a8bef8574ef414f1c6e24753c57aab3be515
dipto0321 <diptokmk47@gmail.com>
2020-07-07 00:33:06 +0600

    :alien: Update access token session time
    - Make it 10 min to 1 day. Now JWT token will be terminate after 1 day

--- src/foddy_recipe_app/settings.py
+++ src/foddy_recipe_app/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {}
 CORS_ORIGIN_ALLOW_ALL = True
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
